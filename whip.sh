f50b4b13bef5f31473460c15702d01690511f7a5
Sebastian Krüger <valknar@pivoine.art>
2025-10-18 14:38:53 +0200

    chore: streamline

--- whip.sh
+++ whip.sh
@@ -7,6 +7,7 @@
 set -euo pipefail
 
 # Colors for output - only use colors if output is to a terminal or if FORCE_COLOR is set
+export FORCE_COLOR=${FORCE_COLOR:-}
 if [[ -z "$FORCE_COLOR" ]]; then
 		if [[ "$FORCE_COLOR" = "1" ]]; then
 			export RED='\033[0;31m'
